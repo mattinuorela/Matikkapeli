45c151522cc1937c6c825c4cedc780d2d5fb0d2e
mattinuorela <c3numa02@students.oamk.fi>
2024-09-28 19:43:14 +0300

    pohjat viety

--- js/functions.js
+++ js/functions.js
@@ -1,50 +1,36 @@
-"use strict"
+const list = document.querySelector('#numbers')
+const additionalList = document.querySelector('#additional-numbers')
 
-let rand_num1 = 0
-let rand_num2 = 0
-let correctAnswerCount = 0
-let incorrectAnswerCount = 0
-
-const getRandomIntNumberInRange = (min,max) => {
-    return Math.floor(Math.random() * max) + min;
+let numbers = []
+let rowIndex = 0;
+while (rowIndex < 7) {
+  const randomNumber = Math.floor(Math.random() * 37 + 1)
+  if (!numbers.includes(randomNumber)) {
+    numbers.push(randomNumber)
+    rowIndex++
+  }
 }
 
-const randomizeNumbers = () => {
-    rand_num1 = getRandomIntNumberInRange(1,10)
-    rand_num2 = getRandomIntNumberInRange(1,10)
-    document.querySelector("#num1").innerHTML = rand_num1
-    document.querySelector("#num2").innerHTML = rand_num2
+numbers.sort((a, b) => a - b);
+const row = list.insertRow()
+for (let i = 0; i < numbers.length; i++) {
+  const col = row.insertCell(i)
+  col.innerHTML = numbers[i]
 }
 
-const updateCorrectAnswerCount = () => {
-    document.querySelector("#correctCount").innerHTML = correctAnswerCount
+let additionalNumbers = []
+let additionalRowIndex = 0
+while (additionalRowIndex < 2) {
+  const randomNumber = Math.floor(Math.random() * 37 + 1)
+  if (!numbers.includes(randomNumber) && !additionalNumbers.includes(randomNumber)) {
+    additionalNumbers.push(randomNumber)
+    additionalRowIndex++
+  }
 }
 
-const updateInCorrectAnswerCount = () => {
-    document.querySelector("#incorrectCount").innerHTML = incorrectAnswerCount
-
+additionalNumbers.sort((a, b) => a - b);
+const additionalRow = additionalList.insertRow()
+for (let i = 0; i < additionalNumbers.length; i++) {
+  const col = additionalRow.insertCell(i)
+  col.innerHTML = additionalNumbers[i]
 }
-
-addEventListener("DOMContentLoaded", () =>{
-    randomizeNumbers()
-})
-
-document.querySelector("button").addEventListener("click",() =>{
-    const answer = Number(document.querySelector("input").value)
-    const correctAnswer = rand_num1 + rand_num2
-    if (answer === correctAnswer) {
-        alert("Correct!")
-        correctAnswerCount++
-        updateCorrectAnswerCount()
-    }   else {
-        alert("Incorrect!")
-        incorrectAnswerCount++
-        updateInCorrectAnswerCount()
-    }
-
-    randomizeNumbers()
-    document.querySelector("input").value=""
-
-    
-
-})
